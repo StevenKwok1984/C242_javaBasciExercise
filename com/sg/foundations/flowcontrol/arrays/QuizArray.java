d488a13a8a532619a5c871c1a64a1f393494c146
Steven Kwok <derekkwok2020@gmail.com>
2022-06-15 14:40:26 +0100

    Exercise Factorizer
    completed

---
+++ com/sg/foundations/flowcontrol/arrays/QuizArray.java
@@ -0,0 +1,30 @@
+package com.sg.foundations.flowcontrol.arrays;
+
+public class QuizArray {
+    public static void main(String[] args){
+        // Q1
+
+        String[] spaceCritters = { "tribble" , "bantha" , "star whale" , "tauntaun" , "babel fish" , "dewback" , "targ" , "toruk" , "porg" };
+        int result = spaceCritters.length;
+        System.out.println(result);
+
+
+        /*
+        // Q2
+
+        String[] fancyFoods = { "gnocchi" , "truffle fries" , "squid ink pasta" ,
+                "escargot" , "unagi" , "turducken" , "jambalaya" , "tamago" , "paella" ,
+                "lobster bisque" , "bavarian cream" , "marzipan" , "spätzle" ,
+                "semifredo" , "chocolate soufflé" , "paella" , "gelato" ,
+                "tiramisù" , "takoyaki" , "okpo ogede" };
+
+        String result = fancyFoods[0];
+        System.out.println(result);
+
+         */
+        // int arr[] = int [6] new;
+        //int arr[];
+        //arr = new int[6];
+        //int [] arr = new int[6];
+    }
+}
