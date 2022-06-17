554eec71b207d5e9d4a01569b9af7780fa21faab
Steven Kwok <derekkwok2020@gmail.com>
2022-06-17 11:20:26 +0100

    Mid-pt test

---
+++ test/Test.java
@@ -0,0 +1,30 @@
+package test;
+
+public class Test {
+    /*
+    public static void main(String args[]) {
+
+        int i = 0, j = 5;
+        for (; (i < 3) && (j++ < 10); i++) {
+            System.out.print(" " + i + " " + j);
+        }
+        System.out.print(" " + i + " " + j);
+    }
+
+    public static void main(String args[]) {
+        int i = 0, j = 2;
+        do {
+            i = ++i;
+            j--;
+        } while (j > 0);
+        System.out.println(i);
+    }
+
+     */
+    public static void main(String args[]) {
+        int x = 3, y = 5, z = 10;
+        // Q20
+        int[] ia = new int[15];
+        System.out.println(++z + y - y + z + x++);
+    }
+}
